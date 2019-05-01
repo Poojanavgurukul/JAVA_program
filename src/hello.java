8906e8d90c0629200155c0724a6457194f04ae83
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-01 16:37:32 +0530

    java_program

---
+++ src/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static  void  main(String args[]){
+        int var=1;
+        while (var<=5) {
+            System.out.println("hello");
+            var+=1;
+        }
+    }
+}
