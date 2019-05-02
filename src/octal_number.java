6691d25deb61fa0f59874d266636445c499fed2f
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-02 11:36:59 +0530

    Refactor.Add functions from variable

--- src/octal_number.java
+++ src/octal_number.java
@@ -7,12 +7,17 @@ public class octal_number {
         System.out.println("You entered " + number);
         int i=0,decimal=0,remainder;
         while(number !=0){
-            remainder=number%10;
+            remainder= getRemainder(number);
             System.out.println(remainder);
-            number=number/10;
-
-
+            number= getQuoitent(number);
         }
     }
-}
 
+    private static int getQuoitent(int number) {
+        return number/10;
+    }
+
+    private static int getRemainder(int number) {
+        return number%10;
+    }
+}
