e08294bf5e526d1147bbbced21210277d20cc72a
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-02 20:17:37 +0530

    getting decimal

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,16 @@
 public class hello {
-    public static  void  main(String args[]){
-        int var=1;
-        while (var<=5) {
-            System.out.println("hello");
-            var+=1;
+
+    public static void main(String[] args) {
+
+        int base = 3, exponent = 2;
+
+        long result = 1;
+
+        for (;exponent != 0; --exponent)
+        {
+            result *= base;
         }
+
+        System.out.println("Answer = " + result);
     }
 }
--- src/octal_number.java
+++ src/octal_number.java
@@ -1,13 +1,15 @@
+import java.sql.SQLOutput;
 
 public class octal_number {
     public  static void main(String args[]){
         int userNumber = Integer.parseInt(args[0]);
-        int i=0,decimal=0,remainder;
+        int i=0,decimal=0,remainder,exponent=8;
         while(userNumber !=0){
             remainder= getRemainder(userNumber);
-            System.out.println(remainder);
             userNumber= getQuoitent(userNumber);
-            System.out.println(userNumber);
+            decimal+=remainder*((int)(Math.pow(exponent,i)));
+            System.out.println(decimal);
+            i++;
         }
     }
     private static int getQuoitent(int number) {
