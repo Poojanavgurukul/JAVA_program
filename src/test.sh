2aee364550df375d2cf9ddb184b1ac29fed81496
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 14:02:07 +0530

    Add.new test case for input 2

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 java OctalToDecimalConverter.java 10 >actual
 diff ExpectedOutput actual
-java OctalToDecimalConverter.java 11 >actual
+java OctalToDecimalConverter.java 2 >actual
 diff ExpectedOutput2 actual
 java OctalToDecimalConverter.java 7 >actual
 diff ExpectedOutput3 actual
