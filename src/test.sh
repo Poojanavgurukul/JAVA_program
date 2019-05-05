c95bff1310f35020b9b7b9607919496024e39994
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 14:09:33 +0530

    Refactor.change the file name ExpectedOutput2 to ExpectedOutputForInput2

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 java OctalToDecimalConverter.java 10 >actual
 diff ExpectedOutputForInput10 actual
 java OctalToDecimalConverter.java 2 >actual
-diff ExpectedOutput2 actual
+diff ExpectedOutputForInput2 actual
 java OctalToDecimalConverter.java 7 >actual
 diff ExpectedOutput3 actual
