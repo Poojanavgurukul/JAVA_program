1fd2d8dcebb396c3d552015f7012c464d49175b4
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 11:31:58 +0530

    Add.boolean varaiable and condition for invalid number or if it is not

--- src/OctalToDecimalConverter.java
+++ src/OctalToDecimalConverter.java
@@ -3,11 +3,21 @@ public class OctalToDecimalConverter {
         String userNumber = args[0];
         String[] number =userNumber.split("");
         int i=0,decimal=0,exponent=8;
+        boolean isOctalNumber=true;
         for (int j =number.length; j>0 ; j--) {
             int octalNumber=Integer.parseInt(number[j-1]);
-            decimal+=octalNumber*((int)(Math.pow(exponent,i)));
-            System.out.println(decimal);
+            if(octalNumber>7){
+                isOctalNumber=false;
+                System.out.println("number is not octal");
+                break;
+            }
+            else{
+                decimal+=octalNumber*((int)(Math.pow(exponent,i)));
+            }
             i++;
+        }
+        if(isOctalNumber==true){
+            System.out.println(decimal);
         }
 
     }
