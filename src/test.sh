ee27c4f8d726c86e06cdbcc20d7674c0632c4b7d
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 14:07:03 +0530

    Refactor.change the file name

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 java OctalToDecimalConverter.java 10 >actual
-diff ExpectedOutput actual
+diff ExpectedOutputForInput10 actual
 java OctalToDecimalConverter.java 2 >actual
 diff ExpectedOutput2 actual
 java OctalToDecimalConverter.java 7 >actual
