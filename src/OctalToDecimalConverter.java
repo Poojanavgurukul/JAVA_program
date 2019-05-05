17700d2069c48d7b4e907fe11a2564460e0dc1b1
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 16:30:17 +0530

    Refactor.validation of octal number outside the loop

--- src/OctalToDecimalConverter.java
+++ src/OctalToDecimalConverter.java
@@ -1,24 +1,20 @@
 public class OctalToDecimalConverter {
     public  static void main(String args[]){
-        int userNumber = Integer.parseInt(args[0]);
+        String number=args[0];
+        if(number.contains("8")||number.contains("9")){
+            System.out.println("number is not octal");
+            return;
+        }
+        int userNumber=Integer.parseInt(args[0]);
         int i=0,decimal=0,remainder,exponent=8;
-        boolean isNotDecimal=true;
         while(userNumber !=0){
             remainder= getRemainder(userNumber);
-            if(remainder>7){
-                isNotDecimal=false;
-                System.out.println("number is not octal");
-                break;
-            }
-            else {
                 userNumber = getQuoitent(userNumber);
                 decimal += remainder * ((int) (Math.pow(exponent, i)));
                 i++;
-            }
         }
-        if (isNotDecimal==true) {
-            System.out.println(decimal);
-        }
+        System.out.println(decimal);
+
     }
     private static int getQuoitent(int number) {
         return number/10;
