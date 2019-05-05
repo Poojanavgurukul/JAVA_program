6d06629c19fa22fad72fcb1c0632626461eb733b
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 11:12:32 +0530

    Add.Print the number one by one in reverse order

--- src/OctalToDecimalConverter.java
+++ src/OctalToDecimalConverter.java
@@ -2,5 +2,9 @@ public class OctalToDecimalConverter {
     public static void main(String args[]) {
         String userNumber = args[0];
         String[] number =userNumber.split("");
+        int i=0;
+        for (int j =number.length; j>0 ; j--) {
+            System.out.println(number[j-1]);
+        }
     }
 }
