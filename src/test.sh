3e7213fe3151fc813765778dbabe866df577c843
Poojanavgurukul <pooja17@navgurukul.org>
2019-05-05 13:58:26 +0530

    Refactor.change the test for 01 to number 7

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,6 @@
 java OctalToDecimalConverter.java 10 >actual
 diff ExpectedOutput actual
-java OctalToDecimalConverter.java 01 >actual
+java OctalToDecimalConverter.java 11 >actual
 diff ExpectedOutput2 actual
+java OctalToDecimalConverter.java 7 >actual
+diff ExpectedOutput3 actual
