c242a52283a58bd36c1caba41c4fd767af5a42f4
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-10 09:20:36 +0530

    Add.variable for calculating length of array and function for getting powers of eight.

--- src/OctalToDecimalConverter.java
+++ src/OctalToDecimalConverter.java
@@ -2,17 +2,19 @@ import java.util.*;
 public class OctalToDecimalConverter {
     public static void main(String args[]) {
         String numberArray = args[0];
+        int length= numberArray.length();
         if (isNotOctal(numberArray)) {
             System.out.println("number is not octal");
             return;
         }
-        int i=0,decimal = 0, exponent = 8;
+        int decimal = 0;
         for(Integer digit :getDigit(args[0])){
-            decimal += digit * ((int) (Math.pow(exponent, i)));
-            i++;
+            decimal += digit;
         }
         System.out.println(decimal);
+        System.out.println(getPowersOfEight(length));
     }
+
     private static List<Integer> getDigit(String Number) {
         int userNumber=Integer.parseInt(Number);
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -22,6 +24,16 @@ public class OctalToDecimalConverter {
             list.add(remainder);
         }
         return list;
+    }
+    private static List<Integer>getPowersOfEight(int length){
+        ArrayList<Integer>listOfPowersOfEight=new ArrayList<Integer>();
+        int i=0,exponent=8;
+        while(i<length){
+            int power=((int)(Math.pow(exponent,i)));
+            listOfPowersOfEight.add(power);
+            i++;
+        }
+        return listOfPowersOfEight;
     }
 
     private static boolean isNotOctal(String number) {
