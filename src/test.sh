9c241d5465f9db49e76c1dcc02057dc3a845ed7c
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-14 12:43:50 +0530

    Refactor.change the name of file

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,8 @@
 java BaseConverter.java --base 8 10 >actual
-diff ExpectedOutputForInput10 actual
+diff ExpectedOutputForInput10Base8 actual
 java BaseConverter.java --base 8 2 >actual
-diff ExpectedOutputForInput2 actual
+diff ExpectedOutputForInput2Base8 actual
 java BaseConverter.java --base 8 3 >actual
-diff ExpectedOutputForInput3 actual
+diff ExpectedOutputForInput3Base8 actual
 java BaseConverter.java --base 8 8 >actual
-diff ExpectedOutputForInput8 actual
+diff ExpectedOutputForInput8Base8 actual
