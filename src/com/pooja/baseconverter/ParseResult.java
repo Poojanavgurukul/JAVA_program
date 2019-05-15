b89bdf38a7073a7a01489f5c6349309f4bee25e0
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-15 13:26:29 +0530

    Refactor.remove the dead variable

--- src/com/pooja/baseconverter/ParseResult.java
+++ src/com/pooja/baseconverter/ParseResult.java
@@ -3,7 +3,7 @@ package com.pooja.baseconverter;
 public class ParseResult {
     public int fromBase;
     public String number;
-    public int toBase;
+
 
     public ParseResult(String[] args) {
         if (args.length==1){
@@ -14,6 +14,5 @@ public class ParseResult {
             this.number = args[2];
             this.fromBase=Integer.parseInt(args[1]);
         }
-        this.toBase=10;
     }
 }
